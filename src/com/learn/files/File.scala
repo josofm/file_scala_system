c2947e1dc61ec360f1e28833f1989fd567e58ea6
Josué Furlan Machado <josue.furlan@peixeurbano.com>
2018-11-12 21:28:37 -0200

    update modified files

--- src/com/learn/files/File.scala
+++ src/com/learn/files/File.scala
@@ -3,7 +3,7 @@ package com.learn.files
 import com.learn.filesystem.FilesystemException
 
 
-class File(override val parentPath: String, override val name: String, contents: String) extends DirEntry(parentPath, name) {
+class File(override val parentPath: String, override val name: String, val contents: String) extends DirEntry(parentPath, name) {
 
   def asDirectory: Directory =
     throw new FilesystemException("File cannot be converted to a directory")
@@ -11,6 +11,15 @@ class File(override val parentPath: String, override val name: String, contents:
   def asFile: File = this
 
   def getType: String = "File"
+
+  def isDirectory: Boolean = false
+  def isFile: Boolean = true
+
+  def setContents(newContents: String): File =
+    new File(parentPath, name, newContents)
+
+  def appendContents(newContents: String): File =
+    setContents(contents + "\n" +  newContents)
 
 }
 
--- src/com/learn/filesystem/FileSystem.scala
+++ src/com/learn/filesystem/FileSystem.scala
@@ -9,6 +9,13 @@ import com.learn.files.Directory
 object FileSystem extends App {
 
   val root = Directory.ROOT
+
+// TODO work this
+//  io.Source.stdin.getLines().foldLeft(State(root, root))((currentState, newLine) => {
+//    currentState.show
+//    Command.from(newLine).apply(currentState)
+//  })
+
   var state =  State(root, root)
   val scanner = new Scanner(System.in)
 
